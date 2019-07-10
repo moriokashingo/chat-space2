92a523a55352f07eb347e930fcf24cf646e321f6
moriokashingo <uma20056@yahoo.co.jp>
2019-07-10 15:27:47 +0900

    delete @group.users << current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,6 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
     @user = current_user
   end
 
@@ -18,7 +17,6 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -28,7 +26,6 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      @group.users << current_user
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
