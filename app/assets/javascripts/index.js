3202f4b6e59efdb386e037385beb40b9c5d9a74f
moriokashingo <uma20056@yahoo.co.jp>
2019-07-13 20:24:30 +0900

    update index.js グループ作成画面において、追加したメンバーとは別のメンバーが追加されてしまう問題

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
 $(function() {
   var preWord;
-  var userdata;
+
 
   function editElement(element) {
     var result = "^" + element;
@@ -15,31 +15,29 @@ $(function() {
                     <p class="chat-group-user__name">${ user.name }</p>
                     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                   </div>`
+
       search_list.append(html)
     }
 
-    function appendNolist() {
-       var html = `<div class="chat-group-user clearfix">
-                     <p class="chat-group-user__name">"一致するユーザーはいません"</p>
-                   </div>`
-        $(search_list).empty() 
-       search_list.append(html)
-       $(search_list).empty() 
-    }
-
 　　//検索後、選択されたユーザーをグループリストに追加
-    function appendUser(user) {
+    function appendUser(name, user_id) {
+      console.log("tata")
       var html = `<div class='chat-group-user clearfix js-chat-member' id='newmember'>
-                    <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
-                    <p class='chat-group-user__name'>${ user.name }</p>
+                    <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                    <p class='chat-group-user__name'>${ name }</p>
                     <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn',id="delete">削除</div>
                   </div>`
       selected_list.append(html)
     }
 
 
-    $(search_list).on("click", '.user-search-add', function() {
-      appendUser(userdata);
+    $(function () {
+      $(document).on("click", '.user-search-add', function () {
+          var name = $(this).attr("data-user-name");
+          var user_id = $(this).attr("data-user-id");
+          $(this).parent().remove();
+          appendUser(name, user_id);
+      });
     });
 
     $(selected_list).on("click", '.js-remove-btn', function() {
@@ -47,7 +45,7 @@ $(function() {
      $(this).parent().detach();
     });
 
-    $("#user-search-field").on("keyup", function() {
+    $("#user-search-field").on("keyup paste", function() {
       var input = $("#user-search-field").val();
       var inputs = input.split(" ").filter(function(e) { return e; });
       var newInputs = inputs.map(editElement);
@@ -68,10 +66,7 @@ $(function() {
               //user検索し、合致するユーザーを表示。後にグループに追加するユーザーを選択する際に使うidにuser.idを指定
               $.each(users, function(i, user) {
                 appendList(user);
-                userdata = user;
               })
-            }else{
-              appendNolist();
             }
           }else{
           $(search_list).empty() 
